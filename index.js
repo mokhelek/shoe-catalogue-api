116f558ddd47d976e619d9157a62e31fd08022cd
mokhelek <mkatleho191@gmail.com>
2023-09-06 00:41:27 +0200

    add route for filtering with both brand & size

--- index.js
+++ index.js
@@ -39,12 +39,41 @@ app.get("/api/shoes/brand/:brandName", async (req, res) => {
 
 app.get("/api/shoes/size/:shoeSize", async (req, res) => {
     try{
-        let shoesListByBrand = await db.any("SELECT * FROM shoes_stock WHERE size = $1", req.params.shoeSize)
-        res.status(200).json(shoesListByBrand);
+        let shoesListBySize = await db.any("SELECT * FROM shoes_stock WHERE size = $1", req.params.shoeSize)
+        res.status(200).json(shoesListBySize);
     }catch(error){
         console.log(error)
     }
 });
+
+app.get("/api/shoes/brand/:brandName/size/:shoeSize", async (req, res) => {
+    try{
+        let shoesListByBrandSize = await db.any("SELECT * FROM shoes_stock WHERE size = $1 AND brand = $2", [req.params.shoeSize, req.params.brandName] )
+        res.status(200).json(shoesListByBrandSize);
+    }catch(error){
+        console.log(error)
+    }
+});
+
+
+app.post("/api/shoes", async (req, res) => {
+    let insertQuery = ``
+    try{
+        console.log(req.body.size, req.body.price)
+        await db.any("SELECT * FROM shoes_stock")
+        res.status(201).json({'message':'Shoes stock successfully updated'});
+
+    }catch(error){
+        console.log(error)
+        res.status(401).send("invalid input")
+    }
+});
+
+
+
+
+
+
 
 
 let PORT = process.env.PORT || 3000;
