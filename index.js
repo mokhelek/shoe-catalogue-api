e3f7f4f7208197aa4620553be340772fd2bd5978
mokhelek <mkatleho191@gmail.com>
2023-09-05 22:57:00 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+import session from "express-session";
+
+let app = express();
+
+
+app.use(
+    session({
+        secret: "<add a secret string here>",
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+app.get("/", (req, res) => {
+    res.send("Something");
+});
+
+
+
+let PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+    console.log("App starting on port", PORT);
+});
