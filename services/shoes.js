3f44729bfe6974bbfe0c94713f3724aef32e1619
mokhelek <mkatleho191@gmail.com>
2023-09-07 16:28:43 +0200

    add fixed sorting

--- services/shoes.js
+++ services/shoes.js
@@ -1,7 +1,7 @@
 export default function shoesService(db) {
     async function getAllShoes(req, res) {
         try {
-            let shoesList = await db.any("SELECT * FROM shoes_stock");
+            let shoesList = await db.any("SELECT * FROM shoes_stock ORDER BY id DESC");
             res.status(200).json(shoesList);
         } catch (error) {
             console.log(error);
@@ -10,7 +10,7 @@ export default function shoesService(db) {
 
     async function getShoesByBrand(req, res) {
         try {
-            let shoesListByBrand = await db.any("SELECT * FROM shoes_stock WHERE brand = $1", req.params.brandName);
+            let shoesListByBrand = await db.any("SELECT * FROM shoes_stock WHERE brand = $1 ORDER BY id DESC", req.params.brandName);
             res.status(200).json(shoesListByBrand);
         } catch (error) {
             console.log(error);
@@ -19,17 +19,16 @@ export default function shoesService(db) {
 
     async function getShoesByColor(req, res) {
         try {
-            let shoesListByColor = await db.any("SELECT * FROM shoes_stock WHERE color = $1", req.params.shoeColor);
+            let shoesListByColor = await db.any("SELECT * FROM shoes_stock WHERE color = $1 ORDER BY id DESC", req.params.shoeColor);
             res.status(200).json(shoesListByColor);
         } catch (error) {
             console.log(error);
         }
     }
 
-
     async function getShoesBySize(req, res) {
         try {
-            let shoesListBySize = await db.any("SELECT * FROM shoes_stock WHERE size = $1", req.params.shoeSize);
+            let shoesListBySize = await db.any("SELECT * FROM shoes_stock WHERE size = $1 ORDER BY id DESC", req.params.shoeSize);
             res.status(200).json(shoesListBySize);
         } catch (error) {
             console.log(error);
@@ -38,7 +37,7 @@ export default function shoesService(db) {
 
     async function getShoesBySizeBrand(req, res) {
         try {
-            let shoesListByBrandSize = await db.any("SELECT * FROM shoes_stock WHERE size = $1 AND brand = $2", [req.params.shoeSize, req.params.brandName]);
+            let shoesListByBrandSize = await db.any("SELECT * FROM shoes_stock WHERE size = $1 AND brand = $2 ORDER BY id DESC", [req.params.shoeSize, req.params.brandName]);
             res.status(200).json(shoesListByBrandSize);
         } catch (error) {
             console.log(error);
@@ -47,7 +46,7 @@ export default function shoesService(db) {
 
     async function getShoesBySizeBrandColor(req, res) {
         try {
-            let shoesListByBrandSizeColor = await db.any("SELECT * FROM shoes_stock WHERE size = $1 AND brand = $2 AND color = $3", [req.params.shoeSize, req.params.brandName, req.params.shoeColor]);
+            let shoesListByBrandSizeColor = await db.any("SELECT * FROM shoes_stock WHERE size = $1 AND brand = $2 AND color = $3 ORDER BY id DESC", [req.params.shoeSize, req.params.brandName, req.params.shoeColor]);
             res.status(200).json(shoesListByBrandSizeColor);
         } catch (error) {
             console.log(error);
@@ -89,6 +88,6 @@ export default function shoesService(db) {
         getShoesBySizeBrand,
         addShoes,
         updateStock,
-        getShoesBySizeBrandColor
+        getShoesBySizeBrandColor,
     };
 }
