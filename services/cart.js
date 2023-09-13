82a7ebc6b1c639b518ef22e369188e225fe702fc
mokhelek <mkatleho191@gmail.com>
2023-09-13 14:42:17 +0200

    combined the cart table with the stock table via inner join

--- services/cart.js
+++ services/cart.js
@@ -1,16 +1,25 @@
-export default function cartService(db){
-
-    async function getCartItems(req, res){
-        try{
-            const cartItems = await db.manyOrNone("SELECT * FROM shopping_cart");
+export default function cartService(db) {
+    async function getCartItems(req, res) {
+        const query = `SELECT 
+                        shoes_stock.id,
+                        shoes_stock.shoe_name,
+                        shoes_stock.brand,
+                        shoes_stock.size,
+                        shoes_stock.price,
+                        shoes_stock.image_url,
+                        shoes_stock.color,
+                        shopping_cart.quantity
+                        FROM shoes_stock
+                        INNER JOIN shopping_cart ON shoes_stock.id = shopping_cart.shoe_id `;
+        try {
+            const cartItems = await db.manyOrNone(query);
             res.status(200).json(cartItems);
-        }catch(error){
-            console.log(error)
+        } catch (error) {
+            console.log(error);
         }
     }
 
-
     return {
-        getCartItems
-    }
+        getCartItems,
+    };
 }
