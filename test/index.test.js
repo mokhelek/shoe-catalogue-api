c6804a9ea8d5345df7b589da6f9aebcc2c38def3
mokhelek <mkatleho191@gmail.com>
2023-09-17 01:29:32 +0200

    added test

--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,29 @@
 import assert from "assert";
 import pgPromise from "pg-promise";
-import "dotenv/config";
+import 'dotenv/config';
+import shoesService from "../services/shoes.js";
 
+
+const connection = process.env.DATABASE_URL_TEST;
+
+const db = pgPromise()(connection);
+db.connect();
+
+
+let shoesServiceInstance = shoesService(db);
 
 describe("The basic database web app", function () {
     this.timeout(10000);
-   
-    it("Testing Tests functionality", async function () {
-
-        assert.equal(1, 1);
+    beforeEach(async function () {
+        try {
+            await db.none("DROP TABLE shopping_cart");
+s        } catch (err) {
+            console.log(err);
+        }
+    });
+    it("Should return a list of all shoes", async function () {
+        let shoes = await shoesServiceInstance.getAllShoes();
+        assert.equal(6, shoes.length);
     });
 
 });
