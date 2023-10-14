8212a79c67b76012ee7d50a0db0bce63428c5257
mokhelek <mkatleho191@gmail.com>
2023-10-14 16:25:21 +0200

    add test for filtering with both size and brand

--- test/index.test.js
+++ test/index.test.js
@@ -65,16 +65,16 @@ describe("The shoe catalogue", function () {
         let shoes = await shoesServiceInstance.getShoesByColor("brown");
         let expectedOutput = [
             {
-                brand: 'GH BASS',
-                color: 'brown',
-                description: 'Lorem ipsum dolor sit amet. Et fugiat fugit sit officia totam in cumque',
+                brand: "GH BASS",
+                color: "brown",
+                description: "Lorem ipsum dolor sit amet. Et fugiat fugit sit officia totam in cumque",
                 id: 2,
-                image_url: 'https://www.johncraig.co.za/wp-content/uploads/BAS02CO-BASS-OCG-WEEJUNS-VENETIAN-LEATHER-BROWN-BA91005E-0CG-V1-600x600.jpg',
-                price: '3000',
-                shoe_name: 'BASS OCG WEEJUNS',
-                size: '6',
-                stock_quantity: 40
-              },
+                image_url: "https://www.johncraig.co.za/wp-content/uploads/BAS02CO-BASS-OCG-WEEJUNS-VENETIAN-LEATHER-BROWN-BA91005E-0CG-V1-600x600.jpg",
+                price: "3000",
+                shoe_name: "BASS OCG WEEJUNS",
+                size: "6",
+                stock_quantity: 40,
+            },
         ];
         assert.deepEqual(expectedOutput, shoes);
     });
@@ -83,19 +83,35 @@ describe("The shoe catalogue", function () {
         let shoes = await shoesServiceInstance.getShoesBySize("8");
         let expectedOutput = [
             {
-                brand: 'Gucci',
-                color: 'green',
-                description: 'Lorem ipsum dolor sit amet. Et fugiat fugit sit officia totam in cumque',
+                brand: "Gucci",
+                color: "green",
+                description: "Lorem ipsum dolor sit amet. Et fugiat fugit sit officia totam in cumque",
                 id: 1,
-                image_url: 'https://catalog-resize-images.thedoublef.com/9b85a5ca98152c0f1e4a1e2538b6ca2e/900/900/3079291M0C0_M_GUC-3154.b.jpg',
-                price: '11000',
-                shoe_name: 'Gucci Loafer',
-                size: '8',
-                stock_quantity: 5
-              }
-            
+                image_url: "https://catalog-resize-images.thedoublef.com/9b85a5ca98152c0f1e4a1e2538b6ca2e/900/900/3079291M0C0_M_GUC-3154.b.jpg",
+                price: "11000",
+                shoe_name: "Gucci Loafer",
+                size: "8",
+                stock_quantity: 5,
+            },
         ];
         assert.deepEqual(expectedOutput, shoes);
     });
 
+    it("Should filter shoes by brand and size", async function () {
+        let shoes = await shoesServiceInstance.getShoesBySizeBrand(["8", "Gucci"]);
+        let expectedOutput = [
+            {
+                brand: "Gucci",
+                color: "green",
+                description: "Lorem ipsum dolor sit amet. Et fugiat fugit sit officia totam in cumque",
+                id: 1,
+                image_url: "https://catalog-resize-images.thedoublef.com/9b85a5ca98152c0f1e4a1e2538b6ca2e/900/900/3079291M0C0_M_GUC-3154.b.jpg",
+                price: "11000",
+                shoe_name: "Gucci Loafer",
+                size: "8",
+                stock_quantity: 5,
+            },
+        ];
+        assert.deepEqual(expectedOutput, shoes);
+    });
 });
